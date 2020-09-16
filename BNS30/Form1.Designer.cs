5c9942fc8ee4bcc42eaf6dd3eb77faaf0f4d264a
RivenVSS <esccapslockctrl@mail.ru>
2020-09-16 10:02:39 +1000

    up

---
+++ BNS30/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace BNS30
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.копироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(12, 418);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(361, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(379, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(390, 449);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "@Ricky";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(12, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(442, 361);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.contextMenuStrip1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.копироватьToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(115, 26);
+            // 
+            // копироватьToolStripMenuItem
+            // 
+            this.копироватьToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Window;
+            this.копироватьToolStripMenuItem.Name = "копироватьToolStripMenuItem";
+            this.копироватьToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.копироватьToolStripMenuItem.Text = "Копировать";
+            this.копироватьToolStripMenuItem.Click += new System.EventHandler(this.копироватьToolStripMenuItem_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Window;
+            this.button3.Location = new System.Drawing.Point(355, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Настройки";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(8, 447);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 19);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "v. 3.01";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Window;
+            this.button2.Location = new System.Drawing.Point(12, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Обновить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(467, 471);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MinimumSize = new System.Drawing.Size(369, 431);
+            this.Name = "Form1";
+            this.Text = "Base Numbers Sump";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem копироватьToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BNS30/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace BNS30 // Base Number Samp 3.0
+{
+    public partial class Form1 : Form
+    {
+        ArrayList Numbers = new ArrayList(); // Динамический массив элементов (номеров Samp)
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Кнопка "Найти"
+        {
+            //listBox1.Items.Add(Convert.ToString(textBox1.Text)); // Добаление строки в список
+            string text = Convert.ToString(textBox1.Text);
+            if (text != "")
+            {
+                for (int i = 1; i < Numbers.Count; i++)
+                {
+                    if (Numbers[i].ToString().Contains(text))
+                    {
+                        listBox1.Items.Add(Numbers[i]);
+                        listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                    }
+                }
+                textBox1.Clear();
+                textBox1.Select(); // Активное поле ввода
+            }
+            else
+            {
+                listBox1.Items.Add("Вы не ввели ник.");
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // При запуске программы
+        {
+            ///*** Поиск, анализ и сортировка номеров в логе ***///
+            try
+            {
+                int New = 0; // Новые элементы
+                Numbers.Clear();
+                string Log = BigDateAbsolute.Properties.Settings.Default.Log; // Путь к лог файлу Samp (берём из настроек)
+                string BNS = BigDateAbsolute.Properties.Settings.Default.BNS; // Путь к базе номеров (берём из настроек)
+                string Buffer; // Промежуточные значения
+                FileStream LogSamp = new FileStream(Log, FileMode.Open); // Открытие файла
+                StreamReader RLogSamp = new StreamReader(LogSamp, Encoding.Default); // Открытие потока для чтения из файла
+                while (!RLogSamp.EndOfStream) // ! - Not , (пока не закончился файл)
+                {
+                    Buffer = RLogSamp.ReadLine(); // Читаем промежуточное значение
+                    if (Buffer.Contains("Контакт: ")) // Имеется ли элемент в строке ?
+                    {
+                        int Index = Buffer.IndexOf("Контакт: ") + 9; // Находим индекс элемента и прибавляем 9 (для исключения элемента "Контакт: ")
+                        int Length = Buffer.Length - Index; // Находим длину нужной подстроки
+                        Buffer = Buffer.Substring(Index, Length); // Присваиваем Buffer подстроку (Начальный индекс, Длина)
+                        Numbers.Add(Buffer);
+                        New++;
+                    }
+                }
+                RLogSamp.Close();
+                FileStream Base = new FileStream(BNS, FileMode.Open); // Открытие файла
+                StreamReader RBase = new StreamReader(Base, Encoding.Default); // Открытие потока для чтения из файла
+                while (!RBase.EndOfStream) // Запись БД номеров в массив
+                {
+                    Buffer = RBase.ReadLine();
+                    Numbers.Add(Buffer);
+                }
+                RBase.Close();
+                int DNumbers = 0; // Счётчик дублей
+                Numbers.Sort(); // Сортировка массива
+                                ///*** Поиск, анализ и сортировка номеров в логе ***///
+                                ///*** Удаление дублей ***///
+                for (int i = 1; i < Numbers.Count; i++)
+                {
+                    if (Numbers[i].ToString() == Numbers[i - 1].ToString())
+                    {
+                        Numbers[i - 1] = "0";
+                        DNumbers++;
+                    }
+                }
+                Numbers.Sort(); // Сортировка массива
+                Numbers.RemoveRange(0, DNumbers); // Удаление нулевых элементов массива в диапазоне (0, DNumbers)
+                                                  ///*** Удаление дублей ***///
+                                                  ///*** Запись в BNS ***///
+                FileStream WriteBase = new FileStream(BNS, FileMode.Create);
+                StreamWriter WBase = new StreamWriter(WriteBase, Encoding.Default);
+                WriteBase.Seek(0, SeekOrigin.End);
+                for (int i = 0; i < Numbers.Count; i++) // Вывод массива в BNS
+                {
+                    WBase.WriteLine(Numbers[i]);
+                }
+                WBase.Close();
+                ///*** Запись в BNS ***///
+                listBox1.Items.Add("Добавление новых элементов завершено.");
+                New = New - DNumbers;
+                listBox1.Items.Add("Всего: " + Numbers.Count + " Новых: " + New);
+                listBox1.Items.Add("Введите ник или часть ника...");
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                textBox1.Select(); // Активное поле ввода
+            } catch
+            {
+                listBox1.Items.Add("Неверно указан путь.");
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e) // Добаление строки в список Enter
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true; // Для корректной работы Enter
+                string text = Convert.ToString(textBox1.Text);
+                if (text != "")
+                {
+                    listBox1.Items.Add("-----------------------------");
+                    listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                    for (int i = 1; i < Numbers.Count; i++)
+                    {
+                        if (Numbers[i].ToString().Contains(text))
+                        {
+                            listBox1.Items.Add(Numbers[i]);
+                            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                        }
+                    }
+                    textBox1.Clear();
+                    textBox1.Select(); // Активное поле ввода
+                }
+                else
+                {
+                    listBox1.Items.Add("Вы не ввели ник.");
+                    listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 Settings = new Form2(); // Объявление формы
+            Settings.Show(); // Вывод формы
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right) // Если нажатая кнопка это ПКМ
+            {
+                if (listBox1.IndexFromPoint(e.X, e.Y) != -1) // Если в точке X,Y есть элемент (-1 если нету)
+                {
+                    listBox1.SetSelected(listBox1.IndexFromPoint(e.X, e.Y), true);  // Выделяем элемент в точке (x,y)
+                    listBox1.ContextMenuStrip.Show(listBox1, new Point(e.X, e.Y)); // Вызываем контекстное меню в точке (x,y)
+                }
+            }
+        }
+
+        private void копироватьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1) // Если есть выделеный элемент
+            {
+                Clipboard.SetText(listBox1.SelectedItem.ToString()); // Копируем в буффер обмена
+            }
+        }
+    }
+}
